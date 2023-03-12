a79a76bb697e1410fe1a66cdd63384f9c7d9330d
Adish <adish.9.gupta@gmail.com>
2023-03-12 19:11:15 +0530

    commit

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,16 +6,19 @@ import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyDJzGgLO__k7s3KOH_own6MEDOg68eFUNc",
+  authDomain: "crwn-clothing-db-ebcf8.firebaseapp.com",
+  projectId: "crwn-clothing-db-ebcf8",
+  storageBucket: "crwn-clothing-db-ebcf8.appspot.com",
+  messagingSenderId: "715223593795",
+  appId: "1:715223593795:web:e847dd517f08aa2368b5f6",
+  measurementId: "G-6JM1CV3BLE"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -28,7 +31,8 @@ googleProvider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () =>
-  signInWithPopup(auth, googleProvider);
+  
+signInWithPopup(auth, googleProvider);
 export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
@@ -74,3 +78,11 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
   return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const signOutUser = async () => {
+  await signOut(auth);
+}
+
+export const onAuthStateChangedListener = (callback) => {
+  onAuthStateChanged(auth,callback);
+}
